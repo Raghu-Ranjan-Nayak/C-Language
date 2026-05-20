74c96fa8dca960351cf2759650f0ced4d201c09e
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-05-20 18:31:21 +0530

    add Terniary operator

---
+++ 02_Instructions_And_Operators/07_Terniary_Operator.c
@@ -0,0 +1,9 @@
+//Check adult or not
+#include<stdio.h>
+int main(){
+    int age;
+    printf("enter age:");
+    scanf("%d",&age);
+    age >= 18 ? printf("adult") : printf("not adult");
+    return 0;
+}
