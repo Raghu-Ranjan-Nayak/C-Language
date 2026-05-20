b8cfa55508402add2ffa343d615602bd4742c95a
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-05-20 16:53:07 +0530

    add input and output

---
+++ 01_Basics_c/02_Input_And_Output.c
@@ -0,0 +1,8 @@
+//Area of a circle
+#include<stdio.h>
+int main(){
+    float radious;
+    printf("Enter a radious:");
+    scanf("%f",&radious);
+    printf("Area of a circle is:%f",3.14*radious);
+}
