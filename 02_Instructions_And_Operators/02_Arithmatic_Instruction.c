6c43b0ef326cea53f29bb1b7e08c34c9d56a192c
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-05-20 17:50:32 +0530

    add arithmatics instruction

---
+++ 02_Instructions_And_Operators/02_Arithmatic_Instruction.c
@@ -0,0 +1,10 @@
+//Average of three numbers
+#include<stdio.h>
+int main(){
+    int a=5;
+    int b=7;
+    int c=9;
+    int average=(a+b+c)/3;
+    printf("Average of three numbers is:%d",average);
+    return 0;
+}
