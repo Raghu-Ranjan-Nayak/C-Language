d076b3217bb4668a77c8ed4900cded23f8b3149c
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-05-25 09:25:05 +0530

    structure variables as argument

---
+++ 10_Structures/02_Structure_Variable_As_Argument.c
@@ -0,0 +1,16 @@
+//Structure variable as argument
+#include<stdio.h>
+struct point{
+    int x;
+    int y;
+};
+void print(struct point p){
+    printf("%d %d \n",p.x,p.y);
+}
+int main(){
+    struct point p1={34,56};
+    struct point p2={45,67};
+    print(p1);
+    print(p2);
+    return 0;
+}
