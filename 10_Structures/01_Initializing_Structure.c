0a51f88b8a19a775844250cea5c87056720787ca
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-05-25 09:11:08 +0530

    structure member as argument

--- 10_Structures/01_Initializing_Structure.c
+++ 10_Structures/01_Initializing_Structure.c
@@ -1,11 +1,16 @@
-//Print name of a student by using structure
+//Print name,roll number and cgpa of a student by using structure
+//Structure member as argument
 #include<stdio.h>
-#include<string.h>
 struct student{
     char name[100];
+    int roll_no;
+    float cgpa;
 };
+void print(char name[],int roll_no,float cgpa){
+    printf("%s %d %f",name,roll_no,cgpa);
+}
 int main(){
-    struct student S1={"Raghu"};
-    printf("student name=%s\n",S1.name);
+    struct student S1={"Raghu",7,9.2};
+    print(S1.name, S1.roll_no, S1.cgpa);
     return 0;
 }
