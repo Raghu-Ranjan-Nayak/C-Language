2682d5f7ce5cd498c8ed55866206235c89749c90
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-06-09 23:41:28 +0530

    create a circular doubly list

--- 11_Linked_Lists/21_Create_A_Node_Of_CDL.c
+++ 11_Linked_Lists/21_Create_A_Node_Of_CDL.c
@@ -1,22 +1,24 @@
-//Create  a anode of the circular singly linked list
+//Create  a anode of the circular doubly linked list
 #include<stdio.h>
 #include<stdlib.h>
 struct node{
+    struct node* prev;
     int data;
     struct node* next;
 };
-struct node* circularsingly(int data);
+struct node* circulardoubly(int data);
 
 int main(){
     struct node* tail;
     int data = 98;
-    tail = circularsingly(data);
+    tail = circulardoubly(data);
 
     printf("%d",tail -> data);
     return 0;
 }
-struct node* circularsingly(int data){
+struct node* circulardoubly(int data){
     struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp -> prev = temp;
     temp -> data = data;
     temp -> next = temp;
     return temp;
