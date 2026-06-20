169ce8dd3be16227b5fae80ed8f301480d2a5fe6
RAGHU RANJAN NAYAK <raghuranjannayak01@gmail.com>
2026-06-20 11:04:17 +0530

    deliting first node

---
+++ 11_Linked_Lists/04_Circular_Doubly_Linked_List/05_Deliting_First_Node.c
@@ -0,0 +1,82 @@
+//Deliting the first node of a circular doubly linked list
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    struct node* prev;
+    int data;
+    struct node* next;
+
+};
+struct node* addatempty(int data);
+struct node* addatend(struct node* tail,int data);
+struct node* delfirst(struct node* tail);
+int print(struct node* tail);
+int main(){
+    struct  node* tail = NULL;
+    tail = addatempty(98);
+    tail = addatend(tail,45);
+    tail = addatend(tail,3);
+
+    printf("List before delition:\n");
+    print(tail);
+
+    tail = delfirst(tail);
+
+    printf("List after delition:\n");
+    print(tail);
+
+    return 0;
+}
+struct node* addatempty(int data){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp -> prev = temp;
+    temp -> data = data;
+    temp -> next = temp;
+
+    return temp;
+}
+struct node* addatend(struct node* tail,int data){
+    struct node* newp = addatempty(data);
+    if(tail == NULL){
+        return newp;
+    }
+    else{
+    struct node* temp =  tail -> next;
+    newp -> next = temp;
+    newp -> prev = tail;
+    tail -> next = newp;
+    temp -> prev = newp;
+    tail = newp;
+
+    return tail;
+    }
+}
+struct node* delfirst(struct node* tail){
+    if(tail == NULL){
+        return tail;
+    }
+    struct node* temp = tail -> next;
+    if(temp == tail){
+        free(tail);
+        tail = NULL;
+        return tail;
+    }
+    tail -> next = temp -> next;
+    temp -> next -> prev = tail;
+    free(temp);
+    return tail;
+}
+int print(struct node* tail){
+    if(tail == NULL){
+        printf("No element in the list");
+    }
+    else{
+        struct node* temp = tail -> next;
+        do{
+            printf("%d \n",temp -> data);
+            temp = temp -> next;
+        }while(temp != tail -> next);
+
+    }
+    printf("\n");
+}
